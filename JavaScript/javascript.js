87ef9470a7aceef8ca6a2e507e5e2dcda5b669fc
ikram <ikrammarbasy17@gmail.com>
2025-01-05 02:02:11 +0700

    Adding a New Project in Section Projects, Adding Images and Moving the Javascript Function for Particle Background Effect to the Html Page

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,23 +1,3 @@
-// Untuk Efek Latar Belakang (Dynamic Particle Background)
-function createParticles() {
-    const parallaxBg = document.getElementById('parallaxBg');
-    for (let i = 0; i < 50; i++) {
-        const particle = document.createElement('div');
-        particle.classList.add('particle');
-        
-        particle.style.width = `${Math.random() * 10}px`;
-        particle.style.height = particle.style.width;
-        particle.style.left = `${Math.random() * 100}%`;
-        particle.style.top = `${Math.random() * 100}%`;
-        particle.style.animationDuration = `${Math.random() * 10 + 5}s`;
-        particle.style.animationDelay = `${Math.random() * 5}s`;
-        
-        parallaxBg.appendChild(particle);
-    }
-}
-
-createParticles();
-
 // Untuk Interaksi dengan Konten Setelah Halaman Dimuat
 document.addEventListener('DOMContentLoaded', () => {
     const skillCards = document.querySelectorAll('.skill-card');
